c4802cff2213e36eb7b91d3b2c34601cdba2c14f
Dave Spina <dspina79@gmail.com>
2022-01-18 20:16:52 -0500

    Began new code to manage data

--- exampledb/manager.js
+++ exampledb/manager.js
@@ -23,3 +23,30 @@ initdb = () => {
     .run("CREATE TABLE IF NOT EXISTS Account (rowid, FirstName text, LastName text, EmailAddress text)")
     .run("CREATE TABLE IF NOT EXISTS Orders (rowid, OrderDate date, Customer integer, FOREIGN KEY(Customer) REFERENCES Account(rowid))")
 }
+
+addAccount = (firstName, lastName, email) => {
+    const sql = `INSERT INTO Account (FirstName, LastName, EmailAddress) VALUES ('${firstName}','${lastName}','${email}')`;
+
+    new sqlite3.Database('./db/crm1.db', (err) => {
+        if (err) {
+            return console.error(err.message);
+        }
+        console.log('Successfully connected to the crm1 database.');
+    }).run(sql);
+}
+
+getAccounts = () => {
+    console.log('Getting accounts');
+    new sqlite3.Database('./db/crm1.db', (err) => {
+        if (err) {
+            return console.error(err.message);
+        }
+        console.log('Successfully connected to the crm1 database.');
+    }).get('SELECT * FROM Account', (error, result) => {
+        if (error) {
+            return console.error(error.message);
+        }
+
+        return result;
+    });;
+}
--- exampledb/testmanager.js
+++ exampledb/testmanager.js
@@ -1,3 +1,8 @@
 const manager = require('./manager');
 
+console.log('Initializing DB');
 initdb();
+console.log('\n\n\n\nAdding Account');
+addAccount("Dean", "Miller", "dean.miller@nowhere.net");
+console.log('\n\n\n\nGetting Accounts');
+console.log(getAccounts());
